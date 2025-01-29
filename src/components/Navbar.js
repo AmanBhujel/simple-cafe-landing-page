224824af1a189e25d35d5cdd6699aa5440c94d4c
AmanBhujel <bhujelaman20@gmail.com>
2025-01-29 13:12:56 +0545

    added name in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="bg-brown-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold text-white">Cozy Café</Link>
+        <Link to="/" className="text-2xl font-bold text-white">The Lost Sip</Link>
         <div className="space-x-4">
           <Link to="/" className="text-white hover:text-gray-300">Home</Link>
           <Link to="/menu" className="text-white hover:text-gray-300">Menu</Link>
